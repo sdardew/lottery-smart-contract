dc73c6385e4e898bdeaa0f57a68aa66d08d08813
sdardew <sdardew@gmail.com>
2021-08-25 00:46:26 -0700

    update test

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -3,19 +3,13 @@ const Lottery = artifacts.require("Lottery");
 // 파라미터에 차례로 0번 주소, 1번 주소, 2번 주소 ... 이렇게 들어온다
 contract('Lottery', function([deployer, user1, user2]){
     let lottery;
-    beforeEach(async () => {
-        console.log('Before each')
 
+    beforeEach(async () => {
         lottery = await Lottery.new();
     })
 
-    it('Basic test', async () => {
-        console.log('Basic test')
-        let owner = await lottery.owner();
-        let value = await lottery.getSomeValue();
-
-        console.log(`owner : ${owner}`);
-        console.log(`value : ${value}`);
-        assert.equal(value, 6)
+    it('getPot should return current pot', async () => {
+        let pot = await lottery.getPot();
+        assert.equal(pot, 0)
     })
 })
