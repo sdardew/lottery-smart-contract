d10b2a8719672ffb948cb071cb0c27cfd03d17cc
sdardew <sdardew@gmail.com>
2021-08-26 00:57:52 -0700

    Implement bet function test

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,9 +1,11 @@
 const Lottery = artifacts.require("Lottery");
-
+const assertRevert = require("./assertRevert");
+const expectEvent = require('./expectEvent');
 // 파라미터에 차례로 0번 주소, 1번 주소, 2번 주소 ... 이렇게 들어온다
 contract('Lottery', function([deployer, user1, user2]){
     let lottery;
-
+    let betAccount = 5 * 10 ** 15;
+    let bet_block_interval = 3;
     beforeEach(async () => {
         lottery = await Lottery.new();
     })
@@ -11,5 +13,38 @@ contract('Lottery', function([deployer, user1, user2]){
     it('getPot should return current pot', async () => {
         let pot = await lottery.getPot();
         assert.equal(pot, 0)
+    })
+
+    describe.only('Bet', function() {
+        it('should fail when the bet money is not 0.005 ETH', async () => {
+            // Fail transaction
+            await assertRevert(lottery.bet('0xab', {from : user1, value: 4000000000000000})) // error를 try-catch로 처리하는지 확인
+
+            // transcation object {chainId, value, to, from, gas(Limit), gasPrice}
+
+        })
+
+        it('should fail when the bet money is not 0.005 ETH', async () => {
+            // bet
+            let receipt = await lottery.bet('0xab', {from : user1, value: betAccount})
+            // console.log(receipt);
+            let pot = await lottery.getPot();
+            assert.equal(pot, 0);
+
+            // check contract balance == 0.005
+            let contractBalance = await web3.eth.getBalance(lottery.address); // truffle은 web3가 자동으로 제공되기 때문에 따로 provider을 사용하지 않아도 됨
+            assert.equal(contractBalance, betAccount)
+
+            // check bet info
+            let currentBlockNumber = await web3.eth.getBlockNumber();
+            let bet = await lottery.getBetInfo(0);
+
+            assert.equal(bet.answerBlockNumber, currentBlockNumber + bet_block_interval);
+            assert.equal(bet.bettor, user1);
+            assert.equal(bet.challenges, '0xab');
+            
+            // check log
+            await expectEvent.inLogs(receipt.logs, 'BET');
+        })
     })
 })
