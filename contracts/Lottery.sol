28f78dcd0df1c5b2919a3520ca9dd9974e44f8bb
sdardew <sdardew@gmail.com>
2021-08-30 00:09:44 -0700

    Add distribute function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -11,18 +11,25 @@ contract Lottery {
     uint256 private _tail;
     uint256 private _head;
     mapping(uint256 => BetInfo) private _bets;
-    address public owner;
+    address payable public owner;
 
     uint256 constant internal BLOCK_LIMIT = 256;
     uint256 constant internal BET_BLOCK_INTERVAL = 3;
     uint256 constant internal BET_AMOUNT = 5 * 10 ** 15; // internal: 내부에서만 -> 10 * 15는 0.001
 
     uint256 private _pot; // 팟머니를 저장
+    bool private mode = false; // false: use answer for test / true: use real block hash
+    bytes32 public answerForTest;
 
     enum BlockStatus {Checkable, NotRevealed, BlockLimitPassed}
     enum BettingResult {Fail, Win, Draw}
 
     event BET(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
+    event WIN(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event FAIL(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event DRAW(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+    event REFUND(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
+
     constructor() public {
         owner = msg.sender;
     }
@@ -52,23 +59,56 @@ contract Lottery {
     }
     
 
-    // Distribute: 검증
+    /**
+     * @dev 베팅 결과를 확인하고 팟머니를 분배한다.
+     * 정답 실패: 팟머니 축척; 정답 맞춤: 맞머니 획득; 한글자 맞춤 OR 정답 확인 불가: 베팅 금액만 획득;
+     */
     function distribute() public {
         uint256 cur;
+        uint256 transferAmount;
+
         BetInfo memory b;
         BlockStatus currentBlockStatus;
+        BettingResult currentBettingResult;
+
         for(cur=_head;cur<_tail; cur++) {
             b = _bets[cur];
             currentBlockStatus = getBlockStatus(b.answerBlockNumber);
 
-            // 정답을 확인하기 위해서는 정답 블록보다 현재 마이닝된 블록이 더 커야 한다
             // Checkable : block.number > AnswerBlockNumber && block.number < BLOCK_LIMIT + AnswerBlockNumber
             if(currentBlockStatus == BlockStatus.Checkable) {
+                bytes32 answerBlockHash = getAnswerBlockHash(b.answerBlockNumber);
+                currentBettingResult = isMatch(b.challenges, answerBlockHash);
+
                 // if win, bettor gets pot
+                if(currentBettingResult == BettingResult.Win) {
+                    // transfer pot
+                    transferAmount = transferAfterPayingFee(b.bettor, _pot + BET_AMOUNT);
+                    
+                    // pot = 0
+                    _pot = 0;
+
+                    // emit WIN
+                    emit WIN(cur, b.bettor, transferAmount, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
 
                 // if fail, bettor's money goes pot
+                if(currentBettingResult == BettingResult.Fail) {
+                    // pot = pot + BET_AMOUNT
+                    _pot += BET_AMOUNT;
+
+                    // emit FAIL
+                    emit FAIL(cur, b.bettor, 0, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
 
                 // if draw, bettor's money
+                if(currentBettingResult == BettingResult.Draw) {
+                    // pot = pot + BET_AMOUNT
+                    transferAmount = transferAfterPayingFee(b.bettor, BET_AMOUNT);
+
+                    // emit DRAW
+                    emit FAIL(cur, b.bettor, transferAmount, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+                }
             }
 
             // Not Revealed: block.number <= AnswerBlockNumber
@@ -79,13 +119,40 @@ contract Lottery {
             // Block Limit Passed: bock.number >= AnswerBlockNumber + BLOCK_LIMIT
             if(currentBlockStatus == BlockStatus.BlockLimitPassed) {
                 // refund
-                // emit refund
-            }
-            popBet(cur);
+                transferAmount = transferAfterPayingFee(b.bettor, BET_AMOUNT);
 
+                // emit refund
+                emit REFUND(cur, b.bettor, transferAmount, b.challenges, b.answerBlockNumber);
+            }
+
+            popBet(cur);
         }
+        _head = cur;
     }
 
+    function transferAfterPayingFee(address payable addr, uint256 amount) internal returns (uint256) {
+        // uint256 fee = amount / 100;
+        uint256 fee = 0; // fee for test
+        uint256 amountWithoutFree;
+
+        // trensfer to addr
+        addr.transfer(amountWithoutFree);
+
+        // transfer to owner
+        owner.transfer(fee);
+
+        return amountWithoutFree;
+    }
+
+    function setAnswerForTest(bytes32 answer) public returns (bool result) {
+        require(msg.sender == owner, "Only owner can set the answer for test mode");
+        answerForTest = answer;
+        return true;
+    }
+
+    function getAnswerBlockHash(uint256 answerBlockNumber) internal view returns (bytes32 answer) {
+        return mode? blockhash(answerBlockNumber) : answerForTest;
+    }
     /**
      * @dev 베팅글자와 정답을 확인한다.
      * @param challenges 베팅글자
